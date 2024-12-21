9fd4e00cfdc74363a83a9e287579fadcb5459bfc
Khalid Ashraf <i.khalidashraf@gmail.com>
2024-12-20 21:50:25 -0500

    made use of loaders and uselocation hook

--- app/routes/userHomepage.tsx
+++ app/routes/userHomepage.tsx
@@ -1,16 +1,20 @@
-import { useParams } from "react-router";
 import type { Route } from "../+types/root";
 
-// export function clientLoader({params}) {
-
-// }
+import { useLocation } from "react-router";
 
 // params keyword will take in the dynamic link that you provided to go to the page
 export default function UserHomepage({ params }: Route.ComponentProps) {
-  console.log(params);
+  // You can pass state from one route to another. You pass the data as an object to state attribute in the Link tag and in the other route, you can receive the data by using the useLocation hook.
+  const location = useLocation();
+  const { user } = location.state;
+
+  console.log(user);
+
   return (
     <div>
-      <p>User: {params.username}</p>
+      <p>User: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Id: {user.id}</p>
     </div>
   );
 }
--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,7 +1,58 @@
-export default function Users() {
+import { Link } from "react-router";
+
+// loader loads data before the component is rendered. There are two types of loaders, normal ones and client
+export async function loader() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const data = await response.json();
+
+  return await response.json();
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default function Users({ loaderData }: { loaderData: User[] }) {
   return (
     <div>
-      <p>Default User Page</p>
+      <h3 className='text-xl font-bold'>Default User Page</h3>
+
+      <ul className='flex flex-col gap-2'>
+        {loaderData.map((user) => {
+          return (
+            <li key={user.id}>
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+                <Link
+                  to={`/users/${user.name}`}
+                  state={{ user }}
+                >
+                  {user.name}
+                </Link>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
